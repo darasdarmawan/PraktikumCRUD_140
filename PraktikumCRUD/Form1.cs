5847d50101c6bb16a78c77fc4b0f1a4aaf7a0c2d
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-03-21 18:25:17 +0700

    menambahkan query untuk menghapus data

--- PraktikumCRUD/Form1.cs
+++ PraktikumCRUD/Form1.cs
@@ -113,6 +113,7 @@ namespace PraktikumCRUD
                         {
                             string nim = dgvMahasiswa.SelectedRows[0].Cells["NIM"].Value.ToString();
                             conn.Open();
+                            string query = "DELETE FROM Mahasiswa WHERE NIM = @NIM";
                         }
                     }
                 }
