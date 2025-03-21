46bac4cd9df29814bd650f29e21c979304598c6e
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-03-21 17:02:48 +0700

    membuat tabel sementara untuk menyimpan data

--- PraktikumCRUD/Form1.cs
+++ PraktikumCRUD/Form1.cs
@@ -42,6 +42,7 @@ namespace PraktikumCRUD
                     conn.Open();
                     string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
                     SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
 
                 }
 
