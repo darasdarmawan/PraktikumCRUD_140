ea6cfbddd16d13f43c1cea7f560933d087b886c3
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-03-21 17:53:46 +0700

    menambahkan exception untuk menampilkan pesan error

--- PraktikumCRUD/Form1.cs
+++ PraktikumCRUD/Form1.cs
@@ -94,6 +94,10 @@ namespace PraktikumCRUD
 
                     } 
                 }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
         }
     }
