de195024c23e6665d03bedf10c37d48118bce8c5
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-03-21 17:11:28 +0700

    menampilkan pesan error jika terjadi kesalahan

--- PraktikumCRUD/Form1.cs
+++ PraktikumCRUD/Form1.cs
@@ -52,7 +52,7 @@ namespace PraktikumCRUD
                 }
                 catch (Exception ex)
                 {
-
+                    MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
 
             }
